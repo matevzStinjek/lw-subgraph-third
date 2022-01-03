508cf590478364705246aabc87b81da7e4a89f24
Matevz Stinjek <stinjek@gmail.com>
2022-01-03 20:40:21 +0000

    refactor

--- src/mappings/leaves.ts
+++ src/mappings/leaves.ts
@@ -19,44 +19,47 @@ export function handleTransfer (event: TransferEvent): void {
     let id = event.address.toHexString().toLowerCase() + "-" + event.params.tokenId.toString();
     let token = Token.load(id);
     if (!token) {    
-      token = new Token(id);
+        token = new Token(id);
+
+        token.tokenID = event.params.tokenId;
+        token.lostWorld = event.address.toHexString();
+        token.minter = event.params.to;
+        token.createdTimestamp = event.block.timestamp.toI32();
+
+        let contract = CurvedRandomLostWorldContract.bind(event.address);  
+        let tokenURIString = contract.tokenURI(event.params.tokenId).slice(22);
+        let tokenURIBytes = Bytes.fromUTF8(tokenURIString) as Bytes;
+        let tokenURI = json.fromBytes(tokenURIBytes).toObject();
+    
+        let name = tokenURI.get("name");
+        if (name) {
+            token.name = name.toString();
+        }
+        let minterLat = tokenURI.get("minterLat");
+        if (minterLat) {
+            token.minterLat = BigDecimal.fromString(minterLat.toF64().toString());
+        }
+        let minterLong = tokenURI.get("minterLat");
+        if (minterLong) {
+            token.minterLat = BigDecimal.fromString(minterLong.toF64().toString());
+        }
+        let image = tokenURI.get("image");
+        if (image) {
+            token.image = image.toString();
+        }
+        let imageLink = tokenURI.get("imageLink");
+        if (imageLink) {
+            token.imageIPFS = imageLink.toString();
+        }
     }
 
-    token.tokenID = event.params.tokenId;
-    token.lostWorld = event.address.toHexString();
-    token.minter = event.params.to;
+    // update owner
     token.owner = event.params.to;
-    token.createdTimestamp = event.block.timestamp.toI32()
+    token.updatedTimestamp = event.block.timestamp.toI32();
 
-    let contract = CurvedRandomLostWorldContract.bind(event.address);
-
-    let tokenURIString = contract.tokenURI(event.params.tokenId).slice(22);
-    let tokenURIBytes = Bytes.fromUTF8(tokenURIString) as Bytes;
-    let tokenURI = json.fromBytes(tokenURIBytes).toObject();
-
-    let name = tokenURI.get("name");
-    if (name) {
-        token.name = name.toString();
-    }
-    let minterLat = tokenURI.get("minterLat");
-    if (minterLat) {
-        token.minterLat = BigDecimal.fromString(minterLat.toF64().toString());
-    }
-    let minterLong = tokenURI.get("minterLat");
-    if (minterLong) {
-        token.minterLat = BigDecimal.fromString(minterLong.toF64().toString());
-    }
-    let image = tokenURI.get("image");
-    if (image) {
-        token.image = image.toString();
-    }
-    let imageLink = tokenURI.get("imageLink");
-    if (imageLink) {
-        token.imageIPFS = imageLink.toString();
-    }
     token.save();
 
-    let lostWorld = LostWorld.load(event.address.toHexString().toLowerCase());
+    let lostWorld = LostWorld.load(event.address.toHexString());
     if (lostWorld && event.params.from == Address.zero()) {
         lostWorld.totalSupply = lostWorld.totalSupply.plus(BigInt.fromI32(1));
         lostWorld.save();
