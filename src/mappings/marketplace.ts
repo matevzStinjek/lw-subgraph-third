1d08bcd9533baf553e30ffd4bcf6e0f6a1ba3341
Matevz Stinjek <stinjek@gmail.com>
2022-03-14 13:42:54 -0500

    feat: adapt thegraph to new architecture and legacy support

--- src/mappings/marketplace.ts
+++ src/mappings/marketplace.ts
@@ -1,48 +1,67 @@
-import {
-    Token,
-    Order,
-} from "../types/schema";
+import { log } from "@graphprotocol/graph-ts"
+
+import { Token, Order } from "../types/schema";
 
 import {
     OrdedAdded as OrderAddedEvent,
     OrderExecuted as OrderExecutedEvent,
     OrderRemoved as OrderRemovedEvent,
-} from "../types/LostWorldsMarketplace/LostWorldsMarketplace";
+} from "../types/AlphaLostWorldsMarketplace/AlphaLostWorldsMarketplace";
 
-export function handleOrderAdded (event: OrderAddedEvent): void {
-    // TODO #1: fix token deletion
-    if (event.params.token.toHexString() == "0x695f4015d80d6e1ceae875373fed8573483525bb") {
-        return;
-    }
-    let order = new Order(event.params.orderId.toString());
-    order.token = event.params.token.toHexString().toLowerCase() + "-" + event.params.tokenId.toString();
-    let token = Token.load(order.token);
-    if (token) {
-        order.tokenName = token.name;
-    }
-    order.lostWorld = event.params.token.toHexString();
+
+export function handleAlphaLostWorldsMarketplaceV1OrderAdded (event: OrderAddedEvent): void {
+    handleOrderAdded(event, "AlphaLostWorldsMarketplaceV1", "V1");
+}
+
+export function handleLostWorldsMarketplaceOrderAdded (event: OrderAddedEvent): void {
+    handleOrderAdded(event, "LostWorldsMarketplace", "M");
+}
+
+function handleOrderAdded (event: OrderAddedEvent, marketplace: string, idPrefix: string): void {    
+    let id = idPrefix + "-" + event.params.orderId.toString();
+    let lostWorld = event.params.token.toHexString().toLowerCase();
+
+    let order = new Order(id);
+    order.orderId = event.params.orderId;
+    order.token = lostWorld + "-" + event.params.tokenId.toString();
+    order.lostWorld = event.params.token.toHexString().toLowerCase();
+    order.marketplace = marketplace;
     order.price = event.params.price;
     order.seller = event.params.seller;
     order.isOpen = true;
     order.isExecuted = false;
     order.createdTimestamp = event.block.timestamp.toI32();
     order.updatedTimestamp = event.block.timestamp.toI32();
-    order.save();
 
+    let token = Token.load(order.token);
     if (token) {
+        order.variation = token.variation;
         token.hasActiveOrder = true;
         token.activeOrder = order.id;
+        token.updatedTimestamp = event.block.timestamp.toI32();
         token.save();
     }
+    order.save();
 }
 
-export function handleOrderExecuted (event: OrderExecutedEvent): void {
-    let order = Order.load(event.params.orderId.toString());
+
+export function handleAlphaLostWorldsMarketplaceV1OrderExecuted (event: OrderExecutedEvent): void {
+    handleOrderExecuted(event, "V1");
+}
+
+export function handleLostWorldsMarketplaceOrderExecuted (event: OrderExecutedEvent): void {
+    handleOrderExecuted(event, "M");
+}
+
+function handleOrderExecuted (event: OrderExecutedEvent, idPrefix: string): void {
+    let id = idPrefix + "-" + event.params.orderId.toString();
+    let order = Order.load(id);
     if (!order) {
         return;
     }
     order.isOpen = false;
     order.isExecuted = true;
+    order.buyer = event.params.buyer;
     order.updatedTimestamp = event.block.timestamp.toI32();
     order.save();
 
@@ -50,12 +69,23 @@ export function handleOrderExecuted (event: OrderExecutedEvent): void {
     if (token) {
         token.hasActiveOrder = false;
         token.activeOrder = null;
+        token.updatedTimestamp = event.block.timestamp.toI32();
         token.save();
     }
 }
 
-export function handleOrderRemoved (event: OrderRemovedEvent): void {
-    let order = Order.load(event.params.orderId.toString());
+
+export function handleAlphaLostWorldsMarketplaceV1OrderRemoved (event: OrderRemovedEvent): void {
+    handleOrderRemoved(event, "V1");
+}
+
+export function handleLostWorldsMarketplaceOrderRemoved (event: OrderRemovedEvent): void {
+    handleOrderRemoved(event, "M");
+}
+
+function handleOrderRemoved (event: OrderRemovedEvent, idPrefix: string): void {
+    let id = idPrefix + "-" + event.params.orderId.toString();
+    let order = Order.load(id);
     if (!order) {
         return;
     }
@@ -67,6 +97,7 @@ export function handleOrderRemoved (event: OrderRemovedEvent): void {
     if (token) {
         token.hasActiveOrder = false;
         token.activeOrder = null;
+        token.updatedTimestamp = event.block.timestamp.toI32();
         token.save();
     }
 }
