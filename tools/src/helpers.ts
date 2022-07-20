a5293ae8fc5926f83737da4049ba2b906d05cfde
Matevz Stinjek <stinjek@gmail.com>
2022-07-20 18:54:07 +0100

    fix(subgraphs): decouple network from config

--- tools/src/helpers.ts
+++ tools/src/helpers.ts
@@ -5,8 +5,9 @@ export const readJson = (path: string) =>
   JSON.parse(fs.readFileSync(`${path}.json`, "utf8"));
 
 export const network = process.env.NETWORK as string;
+export const config = process.env.CONFIG as string;
 export const getContract = (contractName: string) => {
-  console.log(contractName, Object.keys(readJson(path.join(process.env.CONTRACTS as string, network)).contracts))
-  return readJson(path.join(process.env.CONTRACTS as string, network))
+  console.log(contractName, Object.keys(readJson(path.join(process.env.CONTRACTS as string, config)).contracts))
+  return readJson(path.join(process.env.CONTRACTS as string, config))
     .contracts[contractName];
 }
